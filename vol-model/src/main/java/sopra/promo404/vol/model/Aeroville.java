782e64bc2e525636fb694efcc9c0ba5706e69bbb
MYJY <yanis.mounsamy@gmail.com>
2018-08-21 16:08:08 +0200

    add aeroville

---
+++ vol-model/src/main/java/sopra/promo404/vol/model/Aeroville.java
@@ -0,0 +1,69 @@
+package sopra.promo404.vol.model;
+
+import javax.persistence.Transient;
+import javax.persistence.Version;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Aeroville {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Transient
+	private Aeroport aeroport;
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="ville_id")
+	private Ville ville;
+	@Version
+	private int version;
+
+	public Aeroville() {
+		
+	}
+	
+	public Aeroville(Aeroport aeroport, Ville ville)
+	{
+		super();
+		this.aeroport = aeroport;
+		this.ville = ville;
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Aeroport getAeroport() {
+		return aeroport;
+	}
+
+	public void setAeroport(Aeroport aeroport) {
+		this.aeroport = aeroport;
+	}
+
+	public Ville getVille() {
+		return ville;
+	}
+
+	public void setVille(Ville ville) {
+		this.ville = ville;
+	}
+	
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+}
--- vol-model/src/main/java/sopra/promo404/vol/model/Ville.java
+++ vol-model/src/main/java/sopra/promo404/vol/model/Ville.java
@@ -1,13 +1,36 @@
 package sopra.promo404.vol.model;
 
+import javax.persistence.Transient;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(name="ville")
 public class Ville {
 
+	@Id
+	@GeneratedValue
 	private Long id;
+	@Column
 	private String nom;
+	@Column
 	private String codePostal;
+	@Version
+	private int version;
+	
+	@OneToMany(mappedBy = "ville", fetch=FetchType.LAZY)
+	private List<Aeroville> aerovilles = new ArrayList<>();
+	@Transient
 	private List<Aeroport> aeroports = new ArrayList<>();
 
 	public Ville() {
@@ -43,6 +66,22 @@ public class Ville {
 
 	public void setAeroports(List<Aeroport> aeroports) {
 		this.aeroports = aeroports;
+	}
+	
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+	public List<Aeroville> getAerovilles() {
+		return aerovilles;
+	}
+
+	public void setAerovilles(List<Aeroville> aerovilles) {
+		this.aerovilles = aerovilles;
 	}
 
 }
