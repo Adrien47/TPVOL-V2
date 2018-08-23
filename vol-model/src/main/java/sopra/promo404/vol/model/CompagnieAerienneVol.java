68bb5ba0d32cbb4ef4489a8323c7cea0a9e6f170
Adrien <adrien.lavelle@hotmail.fr>
2018-08-23 16:55:30 +0200

    finish test

--- vol-model/src/main/java/sopra/promo404/vol/model/CompagnieAerienneVol.java
+++ vol-model/src/main/java/sopra/promo404/vol/model/CompagnieAerienneVol.java
@@ -1,15 +1,12 @@
 package sopra.promo404.vol.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Transient;
+
 import javax.persistence.Version;
 @Entity
 public class CompagnieAerienneVol {
