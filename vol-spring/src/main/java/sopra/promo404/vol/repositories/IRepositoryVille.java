2a8c0d249e43b2fea763d04dae7849f49ee83f42
Emyfony <emeline.fourny@gmail.com>
2018-08-23 16:13:12 +0200

    repository vol + ville

--- vol-spring/src/main/java/sopra/promo404/vol/repositories/IRepositoryVille.java
+++ vol-spring/src/main/java/sopra/promo404/vol/repositories/IRepositoryVille.java
@@ -3,6 +3,6 @@ import sopra.promo404.vol.model.Ville;
 
 import sopra.promo404.vol.model.Ville;
 
-public interface IDaoVille extends IDao<Ville, Long>{
+public interface IRepositoryVille extends JpaRepository<Ville, Long>{
 	
 }
