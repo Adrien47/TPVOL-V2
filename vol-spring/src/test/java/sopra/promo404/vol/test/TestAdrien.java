e4c4832f6311ba2c83fe2ef3b0b33ab7dc9f214d
MYJY <yanis.mounsamy@gmail.com>
2018-08-23 16:30:05 +0200

    comment test

--- vol-spring/src/test/java/sopra/promo404/vol/test/TestAdrien.java
+++ vol-spring/src/test/java/sopra/promo404/vol/test/TestAdrien.java
@@ -1,54 +1,51 @@
 package sopra.promo404.vol.test;
 
-import sopra.promo404.vol.Application;
 import sopra.promo404.vol.model.CompagnieAerienne;
 import sopra.promo404.vol.model.CompagnieAerienneVol;
 import sopra.promo404.vol.model.Vol;
-import sopra.promo404.vol.repositories.IDaoCompagnieAerienne;
-import sopra.promo404.vol.repositories.IDaoCompagnieAerienneVol;
 import sopra.promo404.vol.repositories.IRepositoryVol;
 
 public class TestAdrien {
 
 	public static void main(String[] args) {
-		IDaoCompagnieAerienne daoCompagnieAerienne = Application.getInstance().getDaoCompagnieAerienne();
-		IDaoCompagnieAerienneVol daoCompagnieAerienneVol = Application.getInstance().getDaoCompagnieAerienneVol();
-		IRepositoryVol daoVol = Application.getInstance().getDaoVol();
-
-		CompagnieAerienne compAer1 = new CompagnieAerienne(666L, "Vol n°1");
-		CompagnieAerienne compAer2 = new CompagnieAerienne(777L, "Vol n°2");
-		CompagnieAerienne compAer3 = new CompagnieAerienne(888L, "Vol n°3");
-
-		CompagnieAerienneVol compAerVol1 = new CompagnieAerienneVol();
-		CompagnieAerienneVol compAerVol2 = new CompagnieAerienneVol();
-		CompagnieAerienneVol compAerVol3 = new CompagnieAerienneVol();
-
-				
-		compAerVol1.setCompagnieAerienne(compAer1);
-//		compAerVol1.setId(123L);
-		compAerVol1.setNumero("ADC");
-
-		
-		compAer1.getVols().add(compAerVol1);
-		compAer1.getVols().add(compAerVol2);
-		compAer1.getVols().add(compAerVol3);
-		
-
-		Vol vol1 = new Vol();
-		Vol vol2 = new Vol();
-		Vol vol3 = new Vol();
-	
-		daoCompagnieAerienne.save(compAer1);
-		daoCompagnieAerienne.save(compAer2);
-		daoCompagnieAerienne.save(compAer3);
-
-		daoVol.save(vol1);
-		daoVol.save(vol2);
-		daoVol.save(vol3);
-
-
-		daoCompagnieAerienneVol.save(compAerVol1);
-		daoCompagnieAerienneVol.save(compAerVol2);
-		daoCompagnieAerienneVol.save(compAerVol3);
+//		IDaoCompagnieAerienne daoCompagnieAerienne = Application.getInstance().getDaoCompagnieAerienne();
+//		IDaoCompagnieAerienneVol daoCompagnieAerienneVol = Application.getInstance().getDaoCompagnieAerienneVol();
+//		IRepositoryVol daoVol = Application.getInstance().getDaoVol();
+//
+//		CompagnieAerienne compAer1 = new CompagnieAerienne(666L, "Vol n°1");
+//		CompagnieAerienne compAer2 = new CompagnieAerienne(777L, "Vol n°2");
+//		CompagnieAerienne compAer3 = new CompagnieAerienne(888L, "Vol n°3");
+//
+//		CompagnieAerienneVol compAerVol1 = new CompagnieAerienneVol();
+//		CompagnieAerienneVol compAerVol2 = new CompagnieAerienneVol();
+//		CompagnieAerienneVol compAerVol3 = new CompagnieAerienneVol();
+//
+//				
+//		compAerVol1.setCompagnieAerienne(compAer1);
+////		compAerVol1.setId(123L);
+//		compAerVol1.setNumero("ADC");
+//
+//		
+//		compAer1.getVols().add(compAerVol1);
+//		compAer1.getVols().add(compAerVol2);
+//		compAer1.getVols().add(compAerVol3);
+//		
+//
+//		Vol vol1 = new Vol();
+//		Vol vol2 = new Vol();
+//		Vol vol3 = new Vol();
+//	
+//		daoCompagnieAerienne.save(compAer1);
+//		daoCompagnieAerienne.save(compAer2);
+//		daoCompagnieAerienne.save(compAer3);
+//
+//		daoVol.save(vol1);
+//		daoVol.save(vol2);
+//		daoVol.save(vol3);
+//
+//
+//		daoCompagnieAerienneVol.save(compAerVol1);
+//		daoCompagnieAerienneVol.save(compAerVol2);
+//		daoCompagnieAerienneVol.save(compAerVol3);
 	}
 }
--- vol-spring/src/test/java/sopra/promo404/vol/test/TestEmeline.java
+++ vol-spring/src/test/java/sopra/promo404/vol/test/TestEmeline.java
@@ -9,7 +9,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import sopra.promo404.vol.Application;
 import sopra.promo404.vol.model.Adresse;
 import sopra.promo404.vol.model.Civilite;
 import sopra.promo404.vol.model.Login;
@@ -17,101 +16,97 @@ import sopra.promo404.vol.model.Particulier;
 import sopra.promo404.vol.model.Passager;
 import sopra.promo404.vol.model.Reservation;
 import sopra.promo404.vol.model.Societe;
-import sopra.promo404.vol.repositories.IDaoClient;
-import sopra.promo404.vol.repositories.IDaoLogin;
-import sopra.promo404.vol.repositories.IDaoPassager;
-import sopra.promo404.vol.repositories.IDaoReservation;
 
 public class TestEmeline {
 	public static void main(String[] args) throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-		IDaoClient daoClient = Application.getInstance().getDaoClient();
-		IDaoPassager daoPassager = Application.getInstance().getDaoPassager();
-		IDaoLogin daoLogin = Application.getInstance().getDaoLogin();
-		IDaoReservation daoReservation = Application.getInstance().getDaoReservation();
-
-		Adresse adresseJeanValjean = new Adresse();
-		Reservation reservationJeanValJean = new Reservation();
-
-		Passager jeanValJean = new Passager();
-		Societe thenardierSarl = new Societe();
-		Login loginThenardier = new Login();
-		Particulier cosette = new Particulier(Civilite.MME, "Cosette");
-		Adresse adresseJavert = new Adresse();
-
-		Login loginCosette = new Login();
-		Adresse adresseCosette = new Adresse();
-		Reservation reservationJavert = new Reservation();
-		Passager javert = new Passager();
-		Societe fantineSarl = new Societe();
-		Particulier gavroche = new Particulier(Civilite.MR, "Gavroche");
-
-		daoPassager.save(javert);
-		daoClient.save(fantineSarl);
-		daoReservation.save(reservationJeanValJean);
-		daoPassager.save(jeanValJean);
-		daoClient.save(gavroche);
-		daoClient.save(cosette);
-		daoLogin.save(loginCosette);
-		daoLogin.save(loginThenardier);
-		daoClient.save(thenardierSarl);
-		daoReservation.save(reservationJavert);
-		
-
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("entityvol");
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
-		adresseJavert.setVille("Paris");
-
-		adresseJeanValjean.setRue("rue de la Chanvrerie ");
-		adresseJeanValjean.setVille("Paris");
-		adresseCosette.setRue("rue des filles du calvaire");
-		adresseCosette.setVille("Paris");
-
-		reservationJeanValJean.setPassager(jeanValJean);
-		reservationJeanValJean.setClient(cosette);
-		reservationJeanValJean.setCode("RF234534");
-		reservationJeanValJean.setConfirmee(true);
-
-		reservationJavert.setPassager(javert);
-		reservationJavert.setClient(cosette);
-		reservationJavert.setConfirmee(false);
-
-		jeanValJean.setAdresse(adresseJeanValjean);
-		jeanValJean.setNom("Valjean");
-		jeanValJean.setPrenom("Jean");
-		jeanValJean.setPieceIdentite("24601");
-		jeanValJean.setReservation(reservationJeanValJean);
-
-		javert.setAdresse(adresseJavert);
-		javert.setNom("Javert");
-		javert.setReservation(reservationJavert);
-
-		loginCosette.setIdentifiant("Coco");
-		loginCosette.setMotDePasse("balai");
-		cosette.setAdresse(adresseCosette);
-		// cosette.getCivilite();
-		cosette.setEmail("cosette@vh.fr");
-		cosette.setNom("Cosette");
-		cosette.setNumeroTel("0123456789");
-		
-		cosette.setLogin(loginCosette);
-
-		thenardierSarl.setEmail("thenard@vh.fr");
-		thenardierSarl.setSiret("34578456700012");
-		loginThenardier.setAdmin(true);
-		loginThenardier.setClient(thenardierSarl);
-		
-		
-		
-		
-		// thenardierSarl.setFormeJuridique(formeJuridique);
-
-		/*
-		 * reservationJeanValJean.setVol(x); reservationJavert.setVol(y);
-		 */
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//
+//		IDaoClient daoClient = Application.getInstance().getDaoClient();
+//		IDaoPassager daoPassager = Application.getInstance().getDaoPassager();
+//		IDaoLogin daoLogin = Application.getInstance().getDaoLogin();
+//		IDaoReservation daoReservation = Application.getInstance().getDaoReservation();
+//
+//		Adresse adresseJeanValjean = new Adresse();
+//		Reservation reservationJeanValJean = new Reservation();
+//
+//		Passager jeanValJean = new Passager();
+//		Societe thenardierSarl = new Societe();
+//		Login loginThenardier = new Login();
+//		Particulier cosette = new Particulier(Civilite.MME, "Cosette");
+//		Adresse adresseJavert = new Adresse();
+//
+//		Login loginCosette = new Login();
+//		Adresse adresseCosette = new Adresse();
+//		Reservation reservationJavert = new Reservation();
+//		Passager javert = new Passager();
+//		Societe fantineSarl = new Societe();
+//		Particulier gavroche = new Particulier(Civilite.MR, "Gavroche");
+//
+//		daoPassager.save(javert);
+//		daoClient.save(fantineSarl);
+//		daoReservation.save(reservationJeanValJean);
+//		daoPassager.save(jeanValJean);
+//		daoClient.save(gavroche);
+//		daoClient.save(cosette);
+//		daoLogin.save(loginCosette);
+//		daoLogin.save(loginThenardier);
+//		daoClient.save(thenardierSarl);
+//		daoReservation.save(reservationJavert);
+//		
+//
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("entityvol");
+//		EntityManager em = emf.createEntityManager();
+//		EntityTransaction tx = em.getTransaction();
+//		tx.begin();
+//		adresseJavert.setVille("Paris");
+//
+//		adresseJeanValjean.setRue("rue de la Chanvrerie ");
+//		adresseJeanValjean.setVille("Paris");
+//		adresseCosette.setRue("rue des filles du calvaire");
+//		adresseCosette.setVille("Paris");
+//
+//		reservationJeanValJean.setPassager(jeanValJean);
+//		reservationJeanValJean.setClient(cosette);
+//		reservationJeanValJean.setCode("RF234534");
+//		reservationJeanValJean.setConfirmee(true);
+//
+//		reservationJavert.setPassager(javert);
+//		reservationJavert.setClient(cosette);
+//		reservationJavert.setConfirmee(false);
+//
+//		jeanValJean.setAdresse(adresseJeanValjean);
+//		jeanValJean.setNom("Valjean");
+//		jeanValJean.setPrenom("Jean");
+//		jeanValJean.setPieceIdentite("24601");
+//		jeanValJean.setReservation(reservationJeanValJean);
+//
+//		javert.setAdresse(adresseJavert);
+//		javert.setNom("Javert");
+//		javert.setReservation(reservationJavert);
+//
+//		loginCosette.setIdentifiant("Coco");
+//		loginCosette.setMotDePasse("balai");
+//		cosette.setAdresse(adresseCosette);
+//		// cosette.getCivilite();
+//		cosette.setEmail("cosette@vh.fr");
+//		cosette.setNom("Cosette");
+//		cosette.setNumeroTel("0123456789");
+//		
+//		cosette.setLogin(loginCosette);
+//
+//		thenardierSarl.setEmail("thenard@vh.fr");
+//		thenardierSarl.setSiret("34578456700012");
+//		loginThenardier.setAdmin(true);
+//		loginThenardier.setClient(thenardierSarl);
+//		
+//		
+//		
+//		
+//		// thenardierSarl.setFormeJuridique(formeJuridique);
+//
+//		/*
+//		 * reservationJeanValJean.setVol(x); reservationJavert.setVol(y);
+//		 */
 
 	}
 }
