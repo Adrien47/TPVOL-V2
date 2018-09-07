dbde518bc435f86fed6156668e03840755cbd2a2
MYJY <yanis.mounsamy@gmail.com>
2018-09-07 16:39:36 +0200

    update passager, ATTENTION Lien Avec réservation non opérationnel pour l'instant

--- vol-spring/src/main/java/sopra/promo404/vol/repositories/IRepositoryReservation.java
+++ vol-spring/src/main/java/sopra/promo404/vol/repositories/IRepositoryReservation.java
@@ -1,11 +1,14 @@
 package sopra.promo404.vol.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 
 import sopra.promo404.vol.model.Reservation;
 
 public interface IRepositoryReservation extends JpaRepository<Reservation, Long> {
-	
+
 }
